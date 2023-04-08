f142762aee1c215be091f429ffe30ff8c59dbd0d
cloudy <cloudy@email.com>
2023-04-08 08:06:50 +0100

    first commit

---
+++ src/api/blog-posts-category/routes/blog-posts-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-posts-category router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blog-posts-category.blog-posts-category');
---
+++ src/api/blog-posts-category/services/blog-posts-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-posts-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::blog-posts-category.blog-posts-category');
